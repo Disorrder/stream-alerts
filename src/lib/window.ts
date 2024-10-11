933efb27b9aadf7cabd330d51722029467244e5a
Disorder <disorrder@gmail.com>
2024-10-11 21:25:58 +0200

    feat: resize app window

---
+++ src/lib/window.ts
@@ -0,0 +1,11 @@
+import { LogicalPosition, LogicalSize, Window } from "@tauri-apps/api/window";
+
+export async function setDefaultWindowSize() {
+  const w = 500;
+  const h = window.screen.height;
+  await Window.getCurrent().setSize(new LogicalSize(w, h));
+
+  // set position to right side of screen
+  const left = window.screen.width - w;
+  await Window.getCurrent().setPosition(new LogicalPosition(left, 0));
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,18 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { useEffect } from "react";
+import { setDefaultWindowSize } from "~/lib/window";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: Root,
+});
+
+function Root() {
+  useEffect(() => {
+    setDefaultWindowSize();
+  }, []);
+
+  return (
     <>
       <div className="flex gap-2 p-2">
         <Link to="/" className="[&.active]:font-bold">
@@ -16,5 +26,5 @@ export const Route = createRootRoute({
       <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  );
+}
