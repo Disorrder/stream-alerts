f9ba06a8821dcfa3eaf8b5a10a39b385bbf6eb81
Disorder <disorrder@gmail.com>
2024-10-11 17:14:57 +0200

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { invoke } from "@tauri-apps/api/core";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Button } from "./components/ui/button";
+import "./index.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,23 +14,7 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank" rel="noreferrer">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+    <div className="_container min-h-screen">
       <form
         className="row"
         onSubmit={(e) => {
@@ -42,7 +27,7 @@ function App() {
           onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
-        <button type="submit">Greet</button>
+        <Button type="submit">Greet</Button>
       </form>
 
       <p>{greetMsg}</p>
