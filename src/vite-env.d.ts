6cb65be12f16dfb55c05fda4c0066d21cf7a6898
Disorder <disorrder@gmail.com>
2024-10-14 02:16:26 +0200

    add twitch styles

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module "*.svg?react" {
+  import type { FunctionComponent, SVGAttributes } from "react";
+  const content: FunctionComponent<SVGAttributes<SVGElement>>;
+  export default content;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig, loadEnv } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
@@ -9,7 +10,7 @@ export default defineConfig(async ({ mode }) => {
   const host = env.TAURI_DEV_HOST;
 
   return {
-    plugins: [TanStackRouterVite(), react()],
+    plugins: [TanStackRouterVite(), react(), svgr()],
     resolve: {
       alias: {
         "~": path.resolve(__dirname, "./src"),
